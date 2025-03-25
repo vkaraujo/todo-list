a2d191d3604466d8166eee8be011126c661a9625
viktor <araujo>
2025-03-24 21:37:46 -0300

    Styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+"use client";
+import React from "react";
+import Form from "./components/Form";
+import Header from "./components/Header";
+import TODOHero from "./components/TODOHero";
+import TODOList from "./components/TODOList";
+//import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function Home() {
+  const [todos, setTodos] = React.useState([]);
 
+  // Retrieve data from localStorage when component mounts
+  React.useEffect(() => {
+    const storedTodos = localStorage.getItem("todos");
+    if (storedTodos) {
+      setTodos(JSON.parse(storedTodos));
+    }
+  }, []);
+
+  const todos_completed = todos.filter(
+    (todo) => todo.is_completed == true
+  ).length;
+  const total_todos = todos.length;
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="wrapper">
+      <Header />
+      <TODOHero todos_completed={todos_completed} total_todos={total_todos} />
+      <Form todos={todos} setTodos={setTodos} />
+      <TODOList todos={todos} setTodos={setTodos} />
+    </div>
+  );
 }
 
-export default App
+export default Home;
